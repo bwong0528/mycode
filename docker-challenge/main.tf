fd86cecff98432ac32b689cdec4a6906c0799259
bolee <bwong8526@gmail.com>
2025-05-29 16:06:11 +0000

    docker challenge

--- docker-challenge/main.tf
+++ docker-challenge/main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+resource "docker_image" "simplegoservice" {
+  name         = "registry.gitlab.com/alta3/simplegoservice"
+  keep_locally = true      // keep image after "destroy"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
+resource "docker_container" "simplegoservice" {
+  image = docker_image.simplegoservice.image_id
   name  = var.container_name
   ports {
     internal = var.internal_port 
